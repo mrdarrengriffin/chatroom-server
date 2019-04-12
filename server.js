5c02a85800f39dfd7100376ec869732a77c36cc6
Darren Griffin <darren.griffin@live.co.uk>
2019-04-12 16:04:47 +0100

    Added basic room joining functionality

--- server.js
+++ server.js
@@ -5,14 +5,36 @@ var port = process.env.PORT || 8001;
 
 
 var app = {
-    rooms:{},
-    users:{}
+    rooms: {},
+    users: {}
 }
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+
+    app.users[socket.id] = {id:socket.id,username:false,room:false}
+    
+    socket.on('joinRoom', data => {
+
+        if (app.rooms[data.room] == undefined) { app.rooms[data.room] = {name:data.room,users: {} } }
+        app.rooms[data.room].users[socket.id] = { username: data.username, id: socket.id }
+
+        app.users[socket.id].username = data.username
+        app.users[socket.id].room = data.room
+        
+        socket.join(data.room)
+        io.to(data.room).emit('userJoinedRoom',app.users[socket.id])
+        
+    })
+
+    socket.on('disconnect', () => {
+        if(app.users[socket.id] == undefined){return}
+        var userRoom = app.rooms[app.users[socket.id].room]
+        socket.leave(userRoom.name)
+        io.to(userRoom.name).emit('userLeftRoom',socket.id)
+    })
 
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
